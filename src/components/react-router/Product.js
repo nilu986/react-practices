96f542a299fccbe3f3890268f63db35ab13f47b8
Anil <anil.yadav@bluejeans.com>
2023-09-06 13:12:49 +0530

    first commit

---
+++ src/components/react-router/Product.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const ProductContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-item: center;
+`;
+
+const Nav = styled.nav`
+    display: flex;
+    width:50%;
+`
+
+const Product = () => {
+  return (
+    <ProductContainer>
+        <Nav>
+            <ul>
+                
+            </ul>
+        </Nav>
+    </ProductContainer>
+  )
+}
+
+export default Product
